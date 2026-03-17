ff33ea1c2321739a1beb6b0b938e225e8f749157
JLBeitz <jesse.beitz@outlook.com>
2026-03-17 01:06:41 -0400

    Implemented moving and idle animations on the main character

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,11 +10,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
 
     private Vector2 moveInput;
+
+    private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,7 +28,19 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("isRunning", true);
+
+        if (context.canceled) // happens when input stops
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+        
+        // The idle logic is before the input direction is read so last input values will not be zero
         moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX",  moveInput.x);
+        animator.SetFloat("InputY",  moveInput.y);
     }
         
 }
