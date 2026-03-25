bffd031f7f0c366db1857f65bcce0af7401cd0a4
JLBeitz <jesse.beitz@outlook.com>
2026-03-25 19:49:09 -0400

    Added build profile logic on startup

---
+++ Assets/Scripts/EnsureDefaultPlatform.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Profile;
+
+[InitializeOnLoad]
+public class ForceStartDemoProfile
+{
+    // Path to build profile
+    private const string ProfilePath = "Assets/Settings/Build Profiles/StartDemo.asset";
+
+    static ForceStartDemoProfile()
+    {
+        // Get the currently active profile
+        BuildProfile activeProfile = BuildProfile.GetActiveBuildProfile();
+
+        // Check if it is already set to StartDemo
+        if (activeProfile == null || activeProfile.name != "StartDemo" && activeProfile.name == "Windows")
+        {
+            // Load the StartDemo asset
+            BuildProfile demoProfile = AssetDatabase.LoadAssetAtPath<BuildProfile>(ProfilePath);
+
+            if (demoProfile != null)
+            {
+                // Switch to it
+                BuildProfile.SetActiveBuildProfile(demoProfile);
+                Debug.Log("Startup override: Active Build Profile forced to StartDemo.");
+            }
+            else
+            {
+                Debug.LogWarning($"ForceStartDemoProfile script couldn't find the Build Profile at: {ProfilePath}. Check the file path!");
+            }
+        }
+    }
+}
