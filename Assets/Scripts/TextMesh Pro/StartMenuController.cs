9d4865638eb402572784ee5898bc383958c9973c
jchiu233 <jchiu233@uwo.ca>
2026-03-26 00:40:40 -0400

    Made it so you can enter a name in the start screen

--- Assets/Scripts/TextMesh Pro/StartMenuController.cs
+++ Assets/Scripts/TextMesh Pro/StartMenuController.cs
@@ -1,15 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro; // Required for TextMeshPro UI elements
 
 public class StartMenuController : MonoBehaviour
 {
+    [Header("UI Elements")]
+   
+    public TMP_InputField nameInputField; 
 
     public void OnStartClick()
     {
- 
+        
+        string playerName = nameInputField.text;
 
+        
+        if (string.IsNullOrWhiteSpace(playerName))
+        {
+            playerName = "Player1";
+        }
 
+        
+        PlayerPrefs.SetString("SavedPlayerName", playerName);
+        PlayerPrefs.Save();
 
+        
         SceneManager.LoadScene("demo");
     }
 
