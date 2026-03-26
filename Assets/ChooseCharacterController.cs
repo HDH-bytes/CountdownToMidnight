4c9c7e041667bab8d529a37e9dc7783149d87195
jchiu233 <jchiu233@uwo.ca>
2026-03-26 05:00:03 -0400

    Change ChooseCharacterController

--- Assets/ChooseCharacterController.cs
+++ Assets/ChooseCharacterController.cs
@@ -1,95 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using TMPro;
 
 public class ChooseCharacterController : MonoBehaviour
 {
-    [Header("Character Data")]
-    public Sprite[] characterSprites;
-    public string[] characterNames;
-
-    [Header("UI")]
-    public Image characterPreviewImage;
-    public TMP_Text characterNameText;
-    public TMP_Text playerNameText;
-
-    private int currentCharacterIndex = 0;
-
-    private void Start()
+    public void ChooseCharacter1()
     {
-        if (characterSprites == null || characterSprites.Length == 0)
-        {
-            Debug.LogError("No character sprites assigned.");
-            return;
-        }
-
-        currentCharacterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
-
-        if (currentCharacterIndex >= characterSprites.Length)
-        {
-            currentCharacterIndex = 0;
-        }
-
-        string playerName = PlayerPrefs.GetString("SavedPlayerName", "Player1");
-
-        if (playerNameText != null)
-        {
-            playerNameText.text = "Player: " + playerName;
-        }
-
-        UpdateCharacterUI();
-    }
-
-    public void NextCharacter()
-    {
-        currentCharacterIndex++;
-
-        if (currentCharacterIndex >= characterSprites.Length)
-        {
-            currentCharacterIndex = 0;
-        }
-
-        UpdateCharacterUI();
-    }
-
-    public void PreviousCharacter()
-    {
-        currentCharacterIndex--;
-
-        if (currentCharacterIndex < 0)
-        {
-            currentCharacterIndex = characterSprites.Length - 1;
-        }
-
-        UpdateCharacterUI();
-    }
-
-    public void ConfirmCharacter()
-    {
-        PlayerPrefs.SetInt("SelectedCharacter", currentCharacterIndex);
+        PlayerPrefs.SetInt("SelectedCharacter", 0);
         PlayerPrefs.Save();
-
         SceneManager.LoadScene("demo");
     }
 
-    private void UpdateCharacterUI()
+    public void ChooseCharacter2()
     {
-        if (characterPreviewImage != null)
-        {
-            characterPreviewImage.sprite = characterSprites[currentCharacterIndex];
-        }
-
-        if (characterNameText != null)
-        {
-            if (characterNames != null && currentCharacterIndex < characterNames.Length)
-            {
-                characterNameText.text = characterNames[currentCharacterIndex];
-            }
-            else
-            {
-                characterNameText.text = "Character " + (currentCharacterIndex + 1);
-            }
-        }
+        PlayerPrefs.SetInt("SelectedCharacter", 1);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("demo");
     }
 }
