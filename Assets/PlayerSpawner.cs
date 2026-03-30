adacca791f954d151d5bef57b819238d3135e279
jchiu233 <jchiu233@uwo.ca>
2026-03-30 13:24:19 -0400

    updated the choose character script

---
+++ Assets/PlayerSpawner.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject character1Prefab;
+    [SerializeField] private GameObject character2Prefab;
+
+    void Start()
+    {
+        // This grabs the exact choice you saved in your ChooseCharacterController
+        int selectedCharacter = PlayerPrefs.GetInt("SelectedCharacter", 0);
+
+        if (selectedCharacter == 0)
+        {
+            // Spawns Character 1 at the Spawner's location
+            Instantiate(character1Prefab, transform.position, transform.rotation);
+        }
+        else if (selectedCharacter == 1)
+        {
+            // Spawns Character 2 at the Spawner's location
+            Instantiate(character2Prefab, transform.position, transform.rotation);
+        }
+    }
+}
