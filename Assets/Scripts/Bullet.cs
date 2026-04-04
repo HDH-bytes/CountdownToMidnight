874d2e1a7c26a3414dfc84653d10c99f7a49665d
victor-liao93 <vliao8@uwo.ca>
2026-04-03 21:55:12 -0400

    Working on gun

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,4 +1,23 @@
 using UnityEngine;
- 
-// placeholder, full implementation coming later
-public class Bullet : MonoBehaviour { }
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject firePoint; 
+        public float fireRate = 1f;
+        private float nextTimeToFire = 0f;
+    
+        void Update()
+        {
+            if (Time.time >= nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + fireRate;
+                Shoot();
+            }
+        }
+    
+        void Shoot()
+        {
+            Instantiate(bulletPrefab, firePoint.transform.position, firePoint.transform.rotation);
+        }
+}
