676296d009e15d7fedbe1c8c1d575d1752c0e586
JLBeitz <jesse.beitz@outlook.com>
2026-04-04 15:51:53 -0400

    Made bullet prefab and script

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,23 +1,16 @@
 using UnityEngine;
 
-public class Gun : MonoBehaviour
-{
-    public GameObject bulletPrefab;
-    public GameObject firePoint; 
-        public float fireRate = 1f;
-        private float nextTimeToFire = 0f;
-    
-        void Update()
-        {
-            if (Time.time >= nextTimeToFire)
-            {
-                nextTimeToFire = Time.time + fireRate;
-                Shoot();
-            }
-        }
-    
-        void Shoot()
-        {
-            Instantiate(bulletPrefab, firePoint.transform.position, firePoint.transform.rotation);
-        }
+public class Bullet : MonoBehaviour
+{   
+    public float bulletSpeed;
+    public float lifeTime;
+
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        
+        rb.linearVelocity = transform.forward * bulletSpeed;
+        
+        Destroy(gameObject, lifeTime);
+    }
 }
