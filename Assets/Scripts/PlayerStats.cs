1f4e2a9577fae7bde2226288a13eb35628b0d797
jchiu233 <jchiu233@uwo.ca>
2026-04-05 04:34:20 -0400

    Added Npc dialogue to Level 5

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Header("Identity")]
+    public string playerName;
+    public string characterName; 
+
+    void Start()
+    {
+        
+        playerName = PlayerPrefs.GetString("SavedPlayerName", "Player 1");
+
+    
+        int characterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
+
+        switch (characterIndex)
+        {
+            case 0:
+                characterName = "Normal Guy"; 
+                break;
+            case 1:
+                characterName = "Ninja";  
+                break;
+            case 2:
+                characterName = "Strong Woman"; 
+                break;
+            default:
+                characterName = "Unknown Class";
+                break;
+        }
+    }
+}
