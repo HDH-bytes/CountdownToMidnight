2b3da1e9b217f26a6cda379a1e23a703d1e6139e
JLBeitz <jesse.beitz@outlook.com>
2026-04-05 13:49:54 -0400

    implemented the knife weapon

--- Assets/Scripts/MeleeWeapon.cs
+++ Assets/Scripts/MeleeWeapon.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 
 public class MeleeWeapon : Weapon
 {
-    public float attackRange;
-    public float damage;
+    public Transform attackPoint; // Tip of the knife
+    public float attackRange = 0.5f;
+    public int damage = 40;
 
     public override void Attack()
     {
-        // Implement knife stab logic later
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(attackPoint.position, attackRange);
+
+        foreach (Collider2D col in hitColliders)
+        {
+            Enemy enemy = col.GetComponent<Enemy>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
         Debug.Log("Stabbing");
     }
 }
