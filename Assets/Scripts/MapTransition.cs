e85cb4c6293f2cf4a5c884f29b29e073906f597e
jchiu233 <jchiu233@uwo.ca>
2026-04-05 00:31:20 -0400

    Started Boss Animations

---
+++ Assets/Scripts/MapTransition.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class MapTransation : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D mapBoundry;
+    CinemachineConfiner confiner;
+
+    private void Awake()
+    {
+        confiner = FindObjectOfType<CinemachineConfiner>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            confiner.m_BoundingShape2D = mapBoundry;
+        }
+    }
+}
