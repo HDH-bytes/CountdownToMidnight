b4a50b638542829a06ec9dbd5bc915daf223627c
JLBeitz <jesse.beitz@outlook.com>
2026-04-08 17:53:25 -0400

    Made weapons unlock upon reaching a set scene

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 //base class, extend to other weapon subclasses
 public abstract class Weapon : MonoBehaviour
 {
+    public int unlockSceneIndex = 1;
     // Each type of weapon will implement this differently
     public abstract void Attack();
 }
--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WeaponSwitcher : MonoBehaviour
 {
     public int selectedWeaponIndex = 0;
-    
-    // NEW: We need to talk to your PlayerAttack script
     public PlayerAttack playerAttackScript;
+    private int currentLevelIndex;
 
     void Start()
     {
-        // If you didn't manually assign the script in the Inspector, find it on the parent (the Player)
         if (playerAttackScript == null)
-        {
             playerAttackScript = GetComponentInParent<PlayerAttack>();
-        }
+
+        // Grab the Build Index of the current active scene
+        currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
 
         SelectWeapon(); 
     }
@@ -23,30 +23,16 @@ public class WeaponSwitcher : MonoBehaviour
         int previousSelectedWeapon = selectedWeaponIndex;
 
         // Number Keys
-        if (Input.GetKeyDown(KeyCode.Alpha1) && transform.childCount >= 1)
-            selectedWeaponIndex = 0;
-        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
-            selectedWeaponIndex = 1;
-        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
-            selectedWeaponIndex = 2;
-        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
-            selectedWeaponIndex = 3;
+        if (Input.GetKeyDown(KeyCode.Alpha1)) TrySelectWeapon(0);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) TrySelectWeapon(1);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) TrySelectWeapon(2);
+        if (Input.GetKeyDown(KeyCode.Alpha4)) TrySelectWeapon(3);
 
         // Mouse Scroll Wheel
         if (Input.GetAxis("Mouse ScrollWheel") > 0f) 
-        {
-            if (selectedWeaponIndex >= transform.childCount - 1)
-                selectedWeaponIndex = 0; 
-            else
-                selectedWeaponIndex++;
-        }
+            ScrollWeapon(1);
         if (Input.GetAxis("Mouse ScrollWheel") < 0f) 
-        {
-            if (selectedWeaponIndex <= 0)
-                selectedWeaponIndex = transform.childCount - 1; 
-            else
-                selectedWeaponIndex--;
-        }
+            ScrollWeapon(-1);
 
         if (previousSelectedWeapon != selectedWeaponIndex)
         {
@@ -54,27 +40,58 @@ public class WeaponSwitcher : MonoBehaviour
         }
     }
 
+    void TrySelectWeapon(int index)
+    {
+        if (index >= transform.childCount) return; 
+
+        Weapon weaponToCheck = transform.GetChild(index).GetComponent<Weapon>();
+        
+        if (weaponToCheck != null && currentLevelIndex >= weaponToCheck.unlockSceneIndex)
+        {
+            selectedWeaponIndex = index;
+        }
+    }
+
+    void ScrollWeapon(int direction)
+    {
+        int checkIndex = selectedWeaponIndex;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            checkIndex += direction;
+
+            if (checkIndex >= transform.childCount) checkIndex = 0;
+            if (checkIndex < 0) checkIndex = transform.childCount - 1;
+
+            Weapon weaponToCheck = transform.GetChild(checkIndex).GetComponent<Weapon>();
+            
+            if (weaponToCheck != null && currentLevelIndex >= weaponToCheck.unlockSceneIndex)
+            {
+                selectedWeaponIndex = checkIndex;
+                return; 
+            }
+        }
+    }
+
     void SelectWeapon()
     {
-        int i = 0;
-        foreach (Transform weapon in transform)
+        for (int i = 0; i < transform.childCount; i++)
         {
+            Transform weaponTransform = transform.GetChild(i);
+
             if (i == selectedWeaponIndex)
             {
-                weapon.gameObject.SetActive(true);  // Turn ON the weapon
+                weaponTransform.gameObject.SetActive(true);  
                 
-                // NEW: Grab the specific Weapon component (Melee or Gun) from this object
-                // and assign it to the PlayerAttack's active slot!
                 if (playerAttackScript != null)
                 {
-                    playerAttackScript.activeWeapon = weapon.GetComponent<Weapon>();
+                    playerAttackScript.activeWeapon = weaponTransform.GetComponent<Weapon>();
                 }
             }
             else
             {
-                weapon.gameObject.SetActive(false); // Turn OFF all others
+                weaponTransform.gameObject.SetActive(false); 
             }
-            i++;
         }
     }
 }
